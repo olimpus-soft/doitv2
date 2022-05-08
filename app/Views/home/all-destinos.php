3d8f24b78720845f5c709a7782f0880ae3ecb873
Miguel Morales <miguelmoralescoterio@gmail.com>
2022-05-08 18:50:35 -0500

    080520221850

--- app/Views/home/all-destinos.php
+++ app/Views/home/all-destinos.php
@@ -9,16 +9,16 @@ if($destinos && is_array($destinos) && count($destinos) > 0 ) {
 ?>
 				<!--====== <?= $destino->destino_titulo; ?> PART START ======-->
 				<div class="col-lg-6 justify-content-center py-4 destino" id="destino-id-<?= $destino->id; ?>">
-					<a target="_self" href="<?= base_url('destino/' . $destino->destino_slug); ?>">
+					<a target="_self" href="<?= base_url('destino/' . $destino->destino_slug.'?utm_source=doit&utm_medium=web&utm_campaign=destinos&utm_term=all&utm_content='.$destino->destino_slug); ?>">
 						<div class="single-project justify-content-center">
 							<div class="project-image justify-content-center">
 									<img src="<?= $destino->destino_image; ?>" alt="<?= $destino->destino_titulo; ?>">
 							</div>
 							<div class="project-content justify-content-center">
-								<a class="project-title" target="_self" href="<?= base_url('destino/' . $destino->destino_slug); ?>">
+								<a class="project-title" target="_self" href="<?= base_url('destino/' . $destino->destino_slug.'?utm_source=doit&utm_medium=web&utm_campaign=destinos&utm_term=all&utm_content='.$destino->destino_slug); ?>">
 									<?= str_replace($replaceViewValues->find2Replace, $replaceViewValues->replace2Found, $destino->destino_titulo); ?>
 								</a>
-								<?= !empty($destino->destino_subtitulo)?'<span class="project-subtitle"><a target="_self" href="'. base_url('destino/' . $destino->destino_slug). '">'.str_replace($replaceViewValues->find2Replace, $replaceViewValues->replace2Found, $destino->destino_subtitulo).'</a></span>':''; ?>
+								<?= !empty($destino->destino_subtitulo)?'<span class="project-subtitle"><a target="_self" href="'. base_url('destino/' . $destino->destino_slug.'?utm_source=doit&utm_medium=web&utm_campaign=destinos&utm_term=all&utm_content='.$destino->destino_slug). '">'.str_replace($replaceViewValues->find2Replace, $replaceViewValues->replace2Found, $destino->destino_subtitulo).'</a></span>':''; ?>
 							</div>
 						</div>
 					</a>
